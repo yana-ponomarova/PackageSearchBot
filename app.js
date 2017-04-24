6944d1d69b5805b2e934dabec5173ea27f00d237
yana-ponomarova <yponomary@gmail.com>
2017-04-24 22:43:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -17,16 +17,16 @@ var spellService = require('./spell-service');
 
 var server = restify.createServer();
 
-/*server.listen(process.env.port || process.env.PORT || 3978, function () {
+server.listen(process.env.port || process.env.PORT || 3978, function () {
 
     console.log('%s listening to %s', server.name, server.url);
 
-});*/
+});
 
 
  // Serve a static web page
- server.get(/.*/, restify.serveStatic({
- 	'directory': '.',
+// server.get(/.*/, restify.serveStatic({
+/* 	'directory': '.',
  	'default': 'index.html'
  }));
  
@@ -34,6 +34,7 @@ var server = restify.createServer();
      console.log('%s listening to %s', server.name, server.url); 
  });
 
+*/
 
 // Create connector and listen for messages
 
